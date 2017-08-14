9f0b51723b7811c5e7016d1b7fee7f5fc3f9743b
phanindrakoka09 <30742576+phanindrakoka09@users.noreply.github.com>
2017-08-14 10:31:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-    res.send('<a href="/">https://avitlearners.blogspot.in/</a>');    
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
     res.send('article two has been requessted');
