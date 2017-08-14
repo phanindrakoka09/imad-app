7511421673db7789932db319d41539082e067ddd
phanindrakoka09 <30742576+phanindrakoka09@users.noreply.github.com>
2017-08-14 20:10:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,10 +11,10 @@ element.innerHTML = 'hai this is done using the javascript';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = mariginLeft + 10;
+    marginLeft = mariginLeft + 1;
     img.style.marginLeft = marginLeft + 'px' ;
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
  
 };
