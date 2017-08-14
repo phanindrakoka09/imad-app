f645f78104d0ea2889e6e9da8c782ad39e90d37d
phanindrakoka09 <30742576+phanindrakoka09@users.noreply.github.com>
2017-08-14 21:02:09 +0530

    [imad-console] Updates ui/maain.js

--- ui/maain.js
+++ ui/maain.js
@@ -1,1 +1,14 @@
-
+//counter code 
+var button = document.getElementBtId('counter');
+button.onclick = function (){
+  
+  //make a request to the counter end point
+  
+  //capture the response and stroe it in a variable
+  
+  //render the variable in the correct span
+  counter = counter + 1;
+  var span =  document.getElementBtId('count');
+  span.innerHTML = counter .toString();
+    
+};
