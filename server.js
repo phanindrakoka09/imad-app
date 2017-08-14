971519daa7c0e214f6086455fe917f6019ca967d
phanindrakoka09 <30742576+phanindrakoka09@users.noreply.github.com>
2017-08-14 13:44:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,9 +87,9 @@ var htmltemplate =
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/articlename', function (req, res) {
-    var articlename=req.params.articlename;
-   res.send(createTemplate(articles[articlename]));
+app.get('/articleName', function (req, res) {
+    var articlename=req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
