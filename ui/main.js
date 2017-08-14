5b926070c88a2d34f10ee19f3a33b894eff77c16
phanindrakoka09 <30742576+phanindrakoka09@users.noreply.github.com>
2017-08-14 19:02:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,8 @@ console.log('Loaded!');
 
 //change the content of main-text
 
-var element = document.getElementById('main-text');
+var element = document.getElementById('main-text'
 
+);
 
 element.innerHtml = 'Hai all this is done using java script';
