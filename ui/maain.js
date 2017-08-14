aefcddf49798e05ca1e7f8170064ea9e6e2f2ba3
phanindrakoka09 <30742576+phanindrakoka09@users.noreply.github.com>
2017-08-14 21:19:54 +0530

    [imad-console] Updates ui/maain.js

--- ui/maain.js
+++ ui/maain.js
@@ -1,15 +1,28 @@
 //counter code 
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function (){
   
   //make a request to the counter end point
+  var request = new XMLHttpRequest();
+  
   
   //capture the response and stroe it in a variable
+  request.onreadystatechange = function (){
+      if(request.readystate === XMLHttpRequest.DONE){
+          //take some action
+          if (request.data === 200){
+              var counter = request.responseText;
+              var span =  document.getElementById('count');
+              span.innerHTML = counter .toString();
+          }
+      }
+      // not done yet 
+  };
   
-  //render the variable in the correct span
-  counter = counter + 1;
-  var span =  document.getElementById('count');
-  span.innerHTML = counter .toString();
+  //make the request 
+  request.open('GET', 'http://phanindrakoka.imad.hasura-app.io/counter',true);
+  request.send(null);
+    
+    
     
 };
